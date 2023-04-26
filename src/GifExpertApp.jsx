51919567a1a45cbe6dc2365fa98587ce87040d1f
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-26 13:11:21 -0500

    add category when a button is pressed

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Dragon Ball', 'Samurai X', 'One Piece'])
+
+  const onAddCategory = () => {
+    setCategories(['HunterXHunter', ...categories])
+  }
+
+  console.log(categories)
+
   return (
     <>
-        <h1>GifExpertApp</h1>
+      {/* Title */}
+      <h1>GifExpertApp</h1>
+
+      {/* Search */}
+      <button onClick={onAddCategory}>Add Category</button>
+
+      {/* List */}
+      <ol>
+        { categories.map((category) => <li key={category}>{category}</li>) }
+      </ol>
     </>
   )
 }
